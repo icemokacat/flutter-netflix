0670f1163cea212b956faa555ade6022c024db92
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-30 13:55:14 +0900

    model 을 이용해 영화 더미 모델 생성

--- flutter_netflix/lib/main.dart
+++ flutter_netflix/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix/screen/home_screen.dart';
 import 'package:flutter_netflix/widget/bottom_bar.dart';
 
 void main() {
@@ -30,11 +31,7 @@ class _MyAppState extends State<MyApp> {
               body: TabBarView(
                 physics: const NeverScrollableScrollPhysics(),
                 children: <Widget>[
-                  Container(
-                    child: Center(
-                      child: Text('home'),
-                    ),
-                  ),
+                  HomeScreen(),
                   Container(
                     child: Center(
                       child: Text('search'),
